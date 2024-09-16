b6b9e6cbdfa13662237b35a4fc0cbbb01301202c
fautaro <fautaro@gmail.com>
2024-09-16 17:48:12 -0300

    Remove metodo no usado

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -55,11 +55,5 @@ namespace Churrasco.Controllers
             return RedirectToAction("Index", "Home");
 
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
